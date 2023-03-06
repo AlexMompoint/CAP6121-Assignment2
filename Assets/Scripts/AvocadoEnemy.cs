0ba18aa4ce3cca524b5f9f87f8c9e5d5640c4dc0
Anthony Mompoint <anthonymompoint379806@gmail.com>
2023-03-06 18:30:22 -0500

    It doens't matter

--- Assets/Scripts/AvocadoEnemy.cs
+++ Assets/Scripts/AvocadoEnemy.cs
@@ -1,39 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AvocadoEnemy : MonoBehaviour
 {
-    public GameObject item;
+    public GameObject cover;
     public Transform center;
+    public Transform target;
+    public GameObject prefab;
+    public Transform[] spawns = new Transform[5];
+
+    private float idleTime = 10;
+    private float nextSwap;
+    private float lastSpawn;
+    private bool changing = true;
     private bool closed = true;
-    public float rotateSpeed = 0.2f;
+    public float rotateSpeed = 0.4f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextSwap = Time.time + idleTime;
+        lastSpawn = Time.time;
     }
 
-    void rotateCover180()
+    bool rotateCover180()
     {
-        item.transform.RotateAround(center.position, Vector3.up, (closed) ? rotateSpeed  : rotateSpeed * -1);
-        if(item.transform.eulerAngles.y > 359.0f)
+        cover.transform.RotateAround(center.position, Vector3.up, (closed) ? rotateSpeed  : rotateSpeed * -1);
+        Debug.Log($"{cover.transform.eulerAngles}");
+        if(cover.transform.localEulerAngles.y > 359.0f)
         {
-            closed = false;
-        } else if (item.transform.eulerAngles.y < 180.0f)
+            return true;
+        } else if (cover.transform.localEulerAngles.y < 180.0f)
         {
-            closed = true;
+            return true;
         }
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
-            
+        if(Time.time > nextSwap)
+        {
+            changing = true;
+        } else if(Time.time > lastSpawn +  1.0f && !closed && !changing)
+        {
+            lastSpawn = Time.time;
+            int RandomNum = Random.Range(0, 5);
+            Instantiate(prefab, spawns[RandomNum].position, transform.rotation);
+        } else if(!closed && !changing) {
+            Transform tempRot = transform;
+            tempRot.LookAt(target, Vector3.up);
+            transform.rotation = Quaternion.Euler(0, tempRot.eulerAngles.y, 0);
+        }
+
     }
 
     private void FixedUpdate()
     {
-        rotateCover180();
+        if(changing && rotateCover180())
+        {
+            Debug.Log("Swapped");
+            closed ^= true;
+            nextSwap = Time.time + idleTime;
+            changing = false;
+        }
     }
 }
